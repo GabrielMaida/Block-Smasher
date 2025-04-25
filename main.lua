ac455fa7280045ab06d509b715ea667d54bb2304
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2025-04-24 22:20:08 -0300

    update: Basics.lua added with introductory concepts

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 function love.draw()
     local largura, altura = love.graphics.getDimensions()
-    love.graphics.print("Resolução atual: " .. largura .. "x" .. altura, largura/2, altura/2)
+    love.graphics.print("Resolução atual: " .. largura .. "x" .. altura, largura / 2, altura / 2)
+    love.graphics.print('Hello World!', 100, 100)
 end
