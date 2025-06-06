85b939f1c959c57ca731dfe96ca90edc0bb5edc4
Gabriel Maida <gabriel.antonio.dlm@gmail.com>
2025-06-05 23:25:39 -0300

    update: added the draw menu function content and changed some variables to use object orientation

--- game/main.lua
+++ game/main.lua
@@ -1,20 +1,32 @@
--- Gabriel Antônio 2025
+-- Block Smasher
+-- Gabriel Antônio - 2025
 ---@diagnostic disable: undefined-global
 ---@diagnostic disable: duplicate-set-field
 
--- Defines the game state as 'menu'
-GameState = "menu" 
+-- Defines the game object
+Game = {
+    name = "Block Smasher",
+    author = "Gabriel Antônio",
+    state = "menu"
+}
 
--- Define the ball as an object
+-- Arguments validation
+if arg[2] == "game" then
+    Game.state = "game"
+elseif arg[2] ~= nil then
+    -- Prints an command line error message
+    io.stderr:write("Error: Invalid argument provided: '" .. arg[2] .. "'.\nUse 'love .' for starting the game or 'love . game' to skip the start menu.\n")
+    -- Exits the game with an error code
+    os.exit(1)
+end
+
+-- Defines the ball as an object
 Ball = {
     x = 0,
     y = 0,
     radius = 15,
     speed = 300
 }
-
--- Defining variables
-local ballCollisionSound, menuBackground
 
 
 -------------------------------------------------------------------
@@ -23,29 +35,38 @@ local ballCollisionSound, menuBackground
 
 -- Main Love Load Function
 function love.load()
-    -- Get width and height dimensions
+    -- Gets the width and height dimensions
     Width, Height = love.graphics.getDimensions()
 
-    -- Load collision sound
-    ballCollisionSound = love.audio.newSource("ball_collision.wav", "static")
+    -- Loads collision sound
+    BallCollisionSound = love.audio.newSource("ball_collision.wav", "static")
 
-    -- If game state is 'menu'
-    if GameState == 'menu' then
-        loadMenu()
-    -- If game state is 'game'
-    elseif GameState == 'game' then
-        loadGame()
+    -- If game state is "menu"
+    if Game.state == "menu" then
+        LoadMenu()
+    -- If game state is "game"
+    elseif Game.state == "game" then
+        LoadGame()
     end
 end
 
 -- Function to load menu assets
-function loadMenu()
-    return 0
+function LoadMenu()
+    -- Loads the background image for the menu
+    MenuBackground = love.graphics.newImage("wallp2.jpeg")
+
+    -- Defines the title object
+    Title = {
+        text = Game.name,
+        font = love.graphics.newFont(80),
+        color = {0, 0, 0, 255}
+    }
+    Title.width = Title.font:getWidth(Title.text)
 end
 
 -- Function to load main game assets
-function loadGame()
-    -- Define the initial ball position
+function LoadGame()
+    -- Defines the initial ball position
     Ball.x = Ball.radius
     Ball.y = Height / 2
 end
@@ -57,77 +78,95 @@ end
 
 -- Main Love Update Function
 function love.update(dt)
-    -- If game state is 'menu'
-    if GameState == 'menu' then
-        updateMenu(dt)
-    -- If game state is 'game'
-    elseif GameState == 'game' then
-        updateGame(dt)
+    -- If game state is "menu"
+    if Game.state == "menu" then
+        UpdateMenu(dt)
+    -- If game state is "game"
+    elseif Game.state == "game" then
+        UpdateGame(dt)
     end
 end
 
 -- Function to update the game menu
-function updateMenu(dt)
+function UpdateMenu(dt)
     return 0
 end
 
 -- Function to update the game itself
-function updateGame(dt)
+function UpdateGame(dt)
     -- Handles horizontal ball movement
     Ball.x = Ball.x + Ball.speed * dt
 
     -- Right Edge Collision
     if Ball.x + Ball.radius > Width then
-        -- Invert ball speed to move left
+        -- Reverses the ball speed to move left
         Ball.speed = -Ball.speed
-        -- Reposition to prevent the ball from sticking
+        -- Repositions to prevent the ball from sticking
         Ball.x = Width - Ball.radius - 1
 
-        -- Play the collision sound
-        ballCollisionSound:play()
+        -- Plays the collision sound
+        BallCollisionSound:play()
     end
 
     -- Left Edge Collision
     if Ball.x - Ball.radius < 0 then
-        -- Invert ball speed to move right
+        -- Reverses the ball speed to move right
         Ball.speed = -Ball.speed
-        -- Reposition to prevent the ball from sticking
+        -- Repositions to prevent the ball from sticking
         Ball.x = Ball.radius + 1
 
-        -- Play the collision sound
-        ballCollisionSound:play()
+        -- Plays the collision sound
+        BallCollisionSound:play()
     end
 end
-
 
 -------------------------------------------------------------------
 -- Draw Section
 
-
 -- Main Love Draw Function
 function love.draw()
-    -- If game state is 'menu'
-    if GameState == 'menu' then
-        drawMenu()
-    -- If game state is 'game'
-    elseif GameState == 'game' then
-        drawGame()
+    -- If game state is "menu"
+    if Game.state == "menu" then
+        DrawMenu()
+    -- If game state is "game"
+    elseif Game.state == "game" then
+        DrawGame()
     end
 end
 
 -- Function to draw the menu assets
-function drawMenu()
-    return 0
+function DrawMenu()
+    -- Checks if there is a background image loaded
+    if MenuBackground then
+        -- Defines the image X and Y axis scales
+        local scaleX = Width / MenuBackground:getWidth()
+        local scaleY = Height / MenuBackground:getHeight()
+        -- Draws the background image scaled to fit the window
+        love.graphics.draw(MenuBackground, 0, 0, 0, scaleX, scaleY)
+    end
+
+    -- Sets the size of the title font
+    love.graphics.setFont(Title.font)
+    -- Sets the color of the title font
+    love.graphics.setColor(Title.color)
+
+    -- Displays the game title horizontally centered
+    love.graphics.print(Title.text, Width / 2 - Title.width / 2, 200)
+
+    -- Resets the color to white
+    love.graphics.setColor(255, 255, 255, 255)
+    -- Resets the font to the default
+    love.graphics.setFont(love.graphics.newFont(12))
 end
 
 -- Function to draw the game assets
-function drawGame()
+function DrawGame()
     -- Draw the ball itself
     love.graphics.circle("line", Ball.x, Ball.y, Ball.radius)
 
     -- Displaying messages
-    love.graphics.print('Block Smasher', 10, 10)
-    love.graphics.print('Hello World!', 150, 10)
+    love.graphics.print(Game.name, 10, 10)
+    love.graphics.print("Hello World!", 150, 10)
 
     -- Show statistics in real time
     love.graphics.print("Ball.X: " .. tostring(Ball.x), 10, 30)
